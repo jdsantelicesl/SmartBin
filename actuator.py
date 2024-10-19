b856c5e84d2c67ebaece841c732f65035442beb0
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-10-19 14:08:34 -0700

    Feature: added function for servo postitions
    Function set_servo_angle() can be called and will take an integer (1-3) to determine servo position

--- actuator.py
+++ actuator.py
@@ -12,7 +12,13 @@ it.start()
 
 servo_pin = board.get_pin("d:9:s")
 
-def set_servo_angle(angle):
-    servo_pin.write(angle)
-    time.sleep(0.015)
+def set_servo_angle(pos):
+    # Three positions of bins. Angles subject to change depending on orientatio
+    if pos == 1:
+        servo_pin.write(30)
+    if pos == 2:
+        servo_pin.write(60)
+    if pos == 3:
+        servo_pin.write(90)
+    time.sleep(0.05) # can move down to 0.015, set to 0.05 as standard
 
